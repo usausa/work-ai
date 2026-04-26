2078d8f04c1984eaf2590a60b9fae27bf67ba630
usausa <machi.pon@gmail.com>
2026-04-26 18:22:45 +0900

    Add

---
+++ Chat/MeaiOllama/Program.cs
@@ -0,0 +1,30 @@
+using MeaiOllama;
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+using OllamaSharp;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.Configure<OllamaOptions>(builder.Configuration.GetSection("Ollama"));
+builder.Services.AddChatClient(p =>
+{
+    var option = p.GetRequiredService<IOptions<OllamaOptions>>().Value;
+    return new OllamaApiClient(new Uri(option.BaseUrl), option.Model);
+});
+
+var app = builder.Build();
+
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+var response = await chatClient.GetResponseAsync("What is .NET? Reply in 50 words max.");
+
+foreach (var message in response.Messages)
+{
+    Console.WriteLine(message.Text);
+}
+
+Console.ReadLine();
